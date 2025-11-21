c90873df49798c20b6a40d0344b28eafb99171d3
Hoanh Le <hoanhle100100@gmail.com>
2025-11-21 16:38:10 +0200

    sampling

--- train_gpt2.py
+++ train_gpt2.py
@@ -71,6 +71,8 @@ class MLP(nn.Module):
         x = self.gelu(x)
         x = self.c_proj(x)
 
+        return x
+
 #----------------------------------------------------------------------------
 
 class Block(nn.Module):
@@ -145,10 +147,78 @@ class GPT(nn.Module):
                 with torch.no_grad():
                     sd[k].copy_(sd_hf[k])
         return model
+    
+    def forward(self, idx): # input is token indices
+        # idx is of shape (B, T)
+        B, T = idx.shape
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+
+        # forward the token and position embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+        tok_emb = self.transformer.wte(idx) # (B, T, C)
+        pos_emb = self.transformer.wpe(pos) # (T, C)
+        x = tok_emb + pos_emb # (B, T, C)
+
+        # forward the transformer blocks
+        for block in self.transformer.h:
+            x = block(x)
+
+        # forward the final layer norm and the classification head
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        return logits
+
 
 
 #----------------------------------------------------------------------------
 
 if __name__ == "__main__":
+    max_return_sequences = 5
+    max_seq_length = 30
     model = GPT.from_pretrained("gpt2")
-    print("model loaded")
+    model.eval()
+    model.to("cuda")
+
+    import tiktoken
+    enc = tiktoken.get_encoding("gpt2")
+    text = "Hello, I'm a language model"
+    tokens = enc.encode(text)
+    tokens = torch.tensor(tokens, dtype=torch.long)
+    tokens = tokens.unsqueeze(0).repeat(max_return_sequences, 1)
+    
+    x = tokens.to("cuda")
+
+    # generate! right now x is (B, T) where B = 5, T = 8
+    # set the seed to 42
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+    torch.cuda.manual_seed_all(42)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    # generate!
+    while x.size(1) < max_seq_length:
+        # forward the model to get the logits for the next token
+        with torch.no_grad():
+            logits = model(x) # (B, T, vocab_size)
+            # get the logits for the last position
+            logits = logits[:, -1, :] # (B, vocab_size)
+            # apply softmax to get the probabilities
+            probs = F.softmax(logits, dim=-1) # (B, vocab_size)
+
+            # do top-k sampling
+            topk_probs, topk_indices = torch.topk(probs, k=50, dim=-1)
+
+            # select a token form top-k probabilities
+            idx_next = torch.multinomial(topk_probs, num_samples=1) # (B, 1)
+
+            # gather the corresponding token indices
+            xcol = torch.gather(topk_indices, dim=-1, index=idx_next) # (B, 1)
+
+            # append the sampled token to the running sequence
+            x = torch.cat((x, xcol), dim=1) # (B, T+1)
+    
+    for i in range(max_return_sequences):
+        tokens = x[i, :max_seq_length].tolist()
+        decoded = enc.decode(tokens)
+        print(decoded)
