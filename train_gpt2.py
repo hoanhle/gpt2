6810d67dd33f8a7c4483ba11a00632ac4fadf8f6
Hoanh Le <hoanhle100100@gmail.com>
2025-11-21 02:09:13 +0200

    add causal self attention and minor docs

--- train_gpt2.py
+++ train_gpt2.py
@@ -2,16 +2,56 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import math
 
 #----------------------------------------------------------------------------
 
 @dataclass
 class GPTConfig:
-    block_size: int = 1024
-    vocab_size: int = 50257
+    block_size: int = 1024 # maximum sequence length
+    vocab_size: int = 50257 # 50000 bpe merges + 256 bytes token + 1 <|endoftext|> token # TODO: watch tokenizer video
     n_layer: int = 12
     n_head: int = 12
     n_embd: int = 384
+
+#----------------------------------------------------------------------------
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=False)
+
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=False)
+
+        # regularization
+        self.n_heads = config.n_head
+        self.n_embd = config.n_embd
+
+        # bias for causal self-attention
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimension (n_embd)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2) # (B, nh, T, hs)
+
+        att = (q @ k.transpose(-2, -1)) * (1 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v # (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+        # output projection
+        y = self.c_proj(y)
+        return y
 
 #----------------------------------------------------------------------------
 
@@ -22,8 +62,10 @@ class MLP(nn.Module):
 
         # gelu activation function: https://arxiv.org/abs/1606.08415
         # reason: https://github.com/pytorch/pytorch/issues/39853
+        # gelu fixes the relu's dead zone problem
         self.gelu = nn.GELU(approximate="tanh")
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd, bias=False)
+
     def forward(self, x):
         x = self.c_fc(x)
         x = self.gelu(x)
